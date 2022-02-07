aa2468c42b522fb3d889bc225c2f3c08581f130b
Barbaros Yildirim <barbarosayildirim@gmail.com>
2022-02-08 00:08:37 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div className="flex justify-center items-center w-screen h-screen bg-gray-700">
+			<div className='w-3/12 p-6 mx-auto rounded text-center bg-blue-500 space-y-5'>
+				<h1 className='text-white text-4xl'>
+					Vite React Tailwind Starter
+				</h1>
+				<p className='text-blue-300 text-2xl'>Hello World?!</p>
+			</div>
+		</div>
+	);
+}
+
+export default App;
